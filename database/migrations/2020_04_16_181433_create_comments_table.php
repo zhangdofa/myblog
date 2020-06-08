c3427b3580722d02d44df978c12b6624a22b463b
zdf <zhangdofa@outlook.com>
2020-06-08 16:57:43 +0800

    文章关联评论、赞

--- database/migrations/2020_04_16_181433_create_comments_table.php
+++ database/migrations/2020_04_16_181433_create_comments_table.php
@@ -17,10 +17,11 @@ class CreateCommentsTable extends Migration
             $table->bigIncrements('id');
             $table->bigInteger('parent_id');
             $table->bigInteger('user_id')->unsigned();
-            $table->bigInteger('contents_id');
+            $table->bigInteger('contents_id')->unsigned();
             $table->text('content');
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('contents_id')->references('id')->on('contents')->onDelete('cascade');
         });
     }
 
--- database/migrations/2020_04_16_182206_create_zans_table.php
+++ database/migrations/2020_04_16_182206_create_zans_table.php
@@ -16,9 +16,10 @@ class CreateZansTable extends Migration
         Schema::create('zans', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->bigInteger('user_id')->unsigned();
-            $table->bigInteger('contents_id');
+            $table->bigInteger('contents_id')->unsigned();
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('contents_id')->references('id')->on('contents')->onDelete('cascade');
         });
     }
 
